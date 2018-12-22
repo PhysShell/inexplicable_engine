48141fe06391e65c52711ea6aab4e7e39214b6ff
PhysShell <schelipov.fedor@yandex.ru>
2018-12-22 10:16:30 +0800

    * NEW ( WINDOWS ): call stack walker class ( from rsdn.ru )

---
+++ inex/core/sources/RpcExStackTraceEngine.cpp
@@ -0,0 +1,395 @@
+#include "stdafx.h"
+#include "RpcExStackTraceEngine.h"
+
+#if IE_PLATFORM_WINDOWS
+
+#include <strsafe.h>
+class CRpcExCritSecLock
+{
+private:
+    LPCRITICAL_SECTION m_cs;
+    bool m_bLocked;
+
+private:
+    //
+    // Private to avoid accidental use.
+    //
+    CRpcExCritSecLock( const CRpcExCritSecLock& );
+    CRpcExCritSecLock& operator=( const CRpcExCritSecLock& );
+
+public:
+    inline CRpcExCritSecLock(
+        ATL::CComAutoCriticalSection* pcs,
+        bool bInitialLock = true) :
+        m_cs(&pcs->m_sec),
+        m_bLocked(false)
+    {
+        if(bInitialLock)
+            Lock();
+    }
+
+    inline CRpcExCritSecLock(
+        ATL::CComAutoCriticalSection& acs,
+        bool bInitialLock = true) :
+        m_cs(&acs.m_sec),
+        m_bLocked(false)
+    {
+        if(bInitialLock)
+            Lock();
+    }
+
+    inline CRpcExCritSecLock(
+        LPCRITICAL_SECTION cs,
+        bool bInitialLock = true) :
+        m_cs(cs),
+        m_bLocked(false)
+    {
+        if(bInitialLock)
+            Lock();
+    }
+
+    inline CRpcExCritSecLock(
+        CRITICAL_SECTION& cs,
+        bool bInitialLock = true) :
+        m_cs(&cs),
+        m_bLocked(false)
+    {
+        if(bInitialLock)
+            Lock();
+    }
+    
+    inline ~CRpcExCritSecLock()
+    {
+        if(m_bLocked)
+            Unlock();
+    }
+
+    inline void Lock()
+    {
+        _ASSERTE(!m_bLocked);
+        EnterCriticalSection(m_cs);
+        m_bLocked = true;
+    }
+
+    inline void Unlock()
+    {
+        _ASSERTE(m_bLocked);
+        LeaveCriticalSection(m_cs);
+        m_bLocked = false;
+    }
+};
+
+RpcExStackTraceEngine::RpcExStackTraceEngine() :
+    m_hDbhHelp(0),
+    m_hProcess(0),
+    pfnSymGetLineFromAddr64(0),
+    pfnSymGetSymFromAddr64(0),
+    pfnSymInitialize(0),
+    pfnSymSetOptions(0),
+    pfnSymGetOptions(0),
+    pfnStackWalk64(0),
+    pfnUnDecorateSymbolName(0),
+    pfnSymFunctionTableAccess64(0),
+    pfnSymGetModuleBase64(0),
+    m_InitializationState(IS_NotPerformed)
+{
+}
+
+HRESULT RpcExStackTraceEngine::Initialize()
+{
+    BOOL Ret;
+
+    CRpcExCritSecLock m_DbgHelpLock(m_DbgHelpGuard);
+    
+    if (m_InitializationState != IS_NotPerformed)
+    {
+        return E_UNEXPECTED;
+    }
+
+    //
+    // Assume initialization is failed.
+    // I'll set this flag to the IS_Done in the
+    // end of this function.
+    //
+    m_InitializationState = IS_Failed;
+
+    //
+    // Load dbghelp.dll dynamically.
+    //
+    if ((m_hDbhHelp = LoadLibrary("dbghelp.dll")) == 0)
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    //
+    // Load procedures.
+    //
+    pfnSymInitialize = (tfnSymInitialize) GetProcAddress(m_hDbhHelp, "SymInitialize" );
+    pfnStackWalk64 = (tfnStackWalk64) GetProcAddress(m_hDbhHelp, "StackWalk64" );
+    pfnSymGetLineFromAddr64 = (tfnSymGetLineFromAddr64) GetProcAddress(m_hDbhHelp, "SymGetLineFromAddr64" );
+    pfnSymGetSymFromAddr64 = (tfnSymGetSymFromAddr64) GetProcAddress(m_hDbhHelp, "SymGetSymFromAddr64" );
+    pfnUnDecorateSymbolName = (tfnUnDecorateSymbolName) GetProcAddress(m_hDbhHelp, "UnDecorateSymbolName" );
+    pfnSymFunctionTableAccess64 = (tfnSymFunctionTableAccess64) GetProcAddress(m_hDbhHelp, "SymFunctionTableAccess64" );
+    pfnSymGetModuleBase64 = (tfnSymGetModuleBase64) GetProcAddress(m_hDbhHelp, "SymGetModuleBase64" );
+    pfnSymSetOptions = (tfnSymSetOptions) GetProcAddress(m_hDbhHelp, "SymSetOptions" );
+    pfnSymGetOptions = (tfnSymGetOptions) GetProcAddress(m_hDbhHelp, "SymGetOptions" );
+
+    if ((pfnSymInitialize == 0) ||
+        (pfnStackWalk64 == 0) ||
+        (pfnSymGetLineFromAddr64 == 0) ||
+        (pfnSymGetSymFromAddr64 == 0) ||
+        (pfnUnDecorateSymbolName == 0) ||
+        (pfnSymFunctionTableAccess64 == 0) ||
+        (pfnSymGetModuleBase64 == 0) ||
+        (pfnSymSetOptions == 0) ||
+        (pfnSymGetOptions == 0))
+    {
+        FreeLibrary(m_hDbhHelp);
+        m_hDbhHelp = 0;
+        return E_UNEXPECTED;
+    }
+
+    DWORD dwOpts = pfnSymGetOptions() ;
+
+    //
+    // Initialize symbols engine.
+    //
+    if ((Ret = pfnSymSetOptions(dwOpts | SYMOPT_LOAD_LINES)) == FALSE)
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    m_hProcess = GetCurrentProcess();
+    
+        
+    // 
+    // SymInitialize 2-nd parameter description from the MSDN.
+    // Pointer to a null-terminated string that specifies a path,
+    // or series of paths separated by a semicolon (;),
+    // that is used to search for symbol files.
+    // If a value of NULL is used, then the library attempts to
+    // form a symbol path from the following sources: 
+    //   The current working directory of the application 
+    //   The _NT_SYMBOL_PATH environment variable 
+    //   The _NT_ALTERNATE_SYMBOL_PATH environment variable 
+    //
+        
+    if ((Ret = pfnSymInitialize(
+        m_hProcess,
+        0,
+        TRUE)) == FALSE)
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    m_InitializationState = IS_Done;
+
+    return S_OK;
+}
+
+RpcExStackTraceEngine::InitializationState RpcExStackTraceEngine::GetInitializationState()
+{
+    return m_InitializationState;
+}
+
+// Declare these so they are accessable anywhere in the project.
+extern "C" void * _ReturnAddress ( void ) ;
+#pragma intrinsic ( _ReturnAddress )
+
+HRESULT RpcExStackTraceEngine::WalkStack(
+    CHAR* StackTrace,
+    DWORD StackTraceSize, /*in characters*/
+    DWORD MaxStackTraceDepth)
+{
+    CRpcExCritSecLock m_DbgHelpLock(m_DbgHelpGuard);
+
+    if ((StackTrace == 0) ||
+        (IsBadWritePtr(StackTrace, StackTraceSize*sizeof(WCHAR))))
+    {
+        //
+        // Grrrr,
+        // I don“t like bad buffers.
+        //
+        return E_INVALIDARG;
+    }
+
+    if (m_InitializationState != IS_Done)
+    {
+        //
+        // Looks like initialization failed
+        // or not performed yet.
+        //
+        return E_UNEXPECTED;
+    }
+
+    //
+    // Init out parameters.
+    //
+    StackTrace[0] = L'\0';
+
+    HANDLE hCurrentThread = GetCurrentThread();
+
+    //
+    // The following should be enough for walking the call stack...
+    //
+    CONTEXT ctx = {0};
+    ctx.ContextFlags = CONTEXT_FULL;
+    
+    __asm    call x;
+    __asm x: pop eax;
+    __asm    mov ctx.Eip, eax;
+    __asm    mov ctx.Ebp, ebp;
+    __asm    mov ctx.Esp, esp;
+
+    //
+    // Init STACKFRAME for the first call
+    //
+    STACKFRAME64 stfrm = {0}; // in/out stackframe
+    stfrm.AddrPC.Offset = ctx.Eip;
+    stfrm.AddrPC.Mode = AddrModeFlat;
+
+    stfrm.AddrStack.Offset = ctx.Esp;
+    stfrm.AddrStack.Mode = AddrModeFlat;
+
+    stfrm.AddrFrame.Offset = ctx.Ebp;
+    stfrm.AddrFrame.Mode = AddrModeFlat;
+
+    //
+    // Symbol info memory buffer.
+    //
+    char SymbolBuffer[65535] = {0};
+    IMAGEHLP_SYMBOL64 *pSymbol = reinterpret_cast<IMAGEHLP_SYMBOL64*>(&SymbolBuffer);
+    pSymbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL64);
+
+    DWORD SymbolNameSize = (DWORD)sizeof(SymbolBuffer) - offsetof(IMAGEHLP_SYMBOL64, Name);
+    pSymbol->MaxNameLength = SymbolNameSize;
+
+    //
+    // Line info.
+    //
+    IMAGEHLP_LINE64 Line = {0};
+    Line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+
+    for(ULONG StackNumber = 0; StackNumber < MaxStackTraceDepth; ++StackNumber)
+    {
+        //
+        // TODO:
+        //    Use heap instead of stack.
+        //    USES_CONVERSION uses stack to store strings.
+        //
+        USES_CONVERSION;
+
+        //
+        // IMAGE_FILE_MACHINE_I386 is only supported now.
+        // I have no other platforms to check this code :(
+        //
+        pfnStackWalk64(
+            IMAGE_FILE_MACHINE_I386,
+            m_hProcess,
+            hCurrentThread,
+            &stfrm,
+            &ctx,
+            0,
+            pfnSymFunctionTableAccess64,
+            pfnSymGetModuleBase64,
+            0);
+
+        if (stfrm.AddrPC.Offset == 0)
+            break;
+
+        //
+        // If StackWalk64 fails it does not cleans ->Name.
+        // We must do this manually.
+        //
+        pSymbol->Name[0] = '\0';
+
+        //
+        // Get procedure info.
+        //
+        DWORD64 SymbolDisplacement = 0;
+        pfnSymGetSymFromAddr64(
+            m_hProcess,
+            stfrm.AddrPC.Offset,
+            &SymbolDisplacement,
+            pSymbol);
+
+        //
+        // Get line info.
+        //
+        DWORD LineDisplacement = 0;
+        pfnSymGetLineFromAddr64(
+            m_hProcess,
+            stfrm.AddrPC.Offset,
+            &LineDisplacement,
+            &Line);
+
+        //
+        // Append symbol name.
+        //
+        if ((pSymbol->Name == 0) ||
+            (pSymbol->Name[0] == '\0'))
+        {
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                "Procedure: <unknown>");
+        }
+        else
+        {
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                "Procedure: ");
+
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                (pSymbol->Name));
+        }
+
+        //
+        // Append symbol location
+        //
+        if ((Line.FileName == 0) ||
+            (Line.FileName[0] == '\0'))
+        {
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                ", File: <unknown>, Line: <unknown>\n");
+        }
+        else
+        {
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                " , File: ");
+
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+               (Line.FileName));
+
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                ", Line: ");
+
+            CHAR LineNumberBuffer[16];
+            _itoa(Line.LineNumber, LineNumberBuffer, 10);
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                LineNumberBuffer);
+
+            StringCchCat(
+                StackTrace,
+                StackTraceSize,
+                "\n");
+        }
+    }
+
+    return S_OK;
+}
+
+#endif // #if IE_PLATFORM_WINDOWS
---
+++ inex/core/sources/RpcExStackTraceEngine.h
@@ -0,0 +1,160 @@
+#if !defined(__RPC_EX_STACK_TRACE_ENGINE_INCLUDED__)
+#define __RPC_EX_STACK_TRACE_ENGINE_INCLUDED__
+
+#if IE_PLATFORM_WINDOWS
+
+#include <Windows.h>
+#include <atlcore.h>
+#include <DbgHelp.h>
+
+// This call stack class is taken from rsdn.ru
+/**
+ * This is class help to obtain call stack as a
+ * simple string. It uses "dbghelp.dll" to walk
+ * the call stack and search symbols in the debug
+ * information. It is recommended to have
+ * only one instance of this class.
+ *
+ * WARNING:
+ *   ASSERT's and TRACE's implementation use
+ *   this class to show call stack, so it is
+ *   not possible to use ASSERT's in this class.
+ */
+class RpcExStackTraceEngine
+{
+public:
+    enum InitializationState
+    {
+        IS_NotPerformed,
+        IS_Failed,
+        IS_Done
+    };
+
+private:
+    /**
+     * HMODULE of the "dbghelp.dll"
+     * We're loading "dbghelp.dll" dynamically to avoid 
+     * dependency from "dbghelp.dll" because if dependency
+     * is present loading RpcExFramework library will fail
+     * if "dbghelp.dll" is absent.
+     */
+    HMODULE m_hDbhHelp;
+
+    /**
+     * Handle of the current process.
+     * We need it in almost all Sym***
+     * operations.
+     */
+    HANDLE m_hProcess;
+
+    /**
+     * RpcExStackTraceEngine initialization state.
+     * We need this because:
+     *   We must do some work in the WalkStack implementation
+     *   only if initialization success.
+     *   We must not do some work in the Initialize only
+     *   if Initialize has not been called before. 
+     */
+    InitializationState m_InitializationState;
+
+    /**
+     * All "dbghelp.dll" functions are single threaded.
+     * So we must protect calls to the "dbghelp.dll"
+     * with critical section.
+     */
+    ATL::CComAutoCriticalSection m_DbgHelpGuard;
+    //
+    // Pointers to the "dbghelp.dll" functions.
+    //
+    
+    // SymGetLineFromAddr64()
+    typedef BOOL (__stdcall *tfnSymGetLineFromAddr64)(
+        HANDLE hProcess,
+        DWORD64 dwAddr,
+        PDWORD pdwDisplacement,
+        PIMAGEHLP_LINE64 Line);
+    tfnSymGetLineFromAddr64 pfnSymGetLineFromAddr64;
+
+    // SymGetSymFromAddr64()
+    typedef BOOL (__stdcall *tfnSymGetSymFromAddr64)(
+        HANDLE hProcess,
+        DWORD64 dwAddr,
+        PDWORD64 pdwDisplacement,
+        PIMAGEHLP_SYMBOL64 Symbol);
+    tfnSymGetSymFromAddr64 pfnSymGetSymFromAddr64;
+
+    // SymInitialize()
+    typedef BOOL (__stdcall *tfnSymInitialize)(
+        HANDLE hProcess,
+        PSTR UserSearchPath,
+        BOOL fInvadeProcess );
+    tfnSymInitialize pfnSymInitialize;
+
+    // SymSetOptions()
+    typedef DWORD (__stdcall *tfnSymSetOptions)(
+        DWORD SymOptions);
+    tfnSymSetOptions pfnSymSetOptions;
+
+    // SymGetOptions()
+    typedef DWORD (__stdcall *tfnSymGetOptions)();
+    tfnSymGetOptions pfnSymGetOptions;
+
+    // StackWalk64()
+    typedef BOOL (__stdcall *tfnStackWalk64)( 
+        DWORD MachineType, 
+        HANDLE hProcess,
+        HANDLE hThread, 
+        LPSTACKFRAME64 StackFrame, 
+        PVOID ContextRecord,
+        PREAD_PROCESS_MEMORY_ROUTINE64 ReadMemoryRoutine,
+        PFUNCTION_TABLE_ACCESS_ROUTINE64 FunctionTableAccessRoutine,
+        PGET_MODULE_BASE_ROUTINE64 GetModuleBaseRoutine,
+        PTRANSLATE_ADDRESS_ROUTINE64 TranslateAddress );
+    tfnStackWalk64 pfnStackWalk64;
+
+    // UnDecorateSymbolName()
+    typedef DWORD (__stdcall WINAPI *tfnUnDecorateSymbolName)(
+        PCSTR DecoratedName,
+        PSTR UnDecoratedName,
+        DWORD UndecoratedLength,
+        DWORD Flags);
+    tfnUnDecorateSymbolName pfnUnDecorateSymbolName;
+    
+    // SymFunctionTableAccess64()
+    typedef PVOID (__stdcall *tfnSymFunctionTableAccess64)(
+        HANDLE hProcess,
+        DWORD64 AddrBase);
+    tfnSymFunctionTableAccess64 pfnSymFunctionTableAccess64;
+
+    // SymGetModuleBase64()
+    typedef DWORD64 (__stdcall *tfnSymGetModuleBase64)(
+        HANDLE hProcess,
+        DWORD64 dwAddr );
+    tfnSymGetModuleBase64 pfnSymGetModuleBase64;
+
+public:
+    RpcExStackTraceEngine();
+
+    /**
+     * Initializes the RpcExStackTraceEngine().
+     * In fact we just initializing symbols engine.
+     */
+    HRESULT Initialize();
+
+    /**
+     * Returns result of the Initialize() call.
+     */
+    InitializationState GetInitializationState();
+
+    /**
+     * Returns stack trace as a string.
+     */
+    HRESULT WalkStack(
+        CHAR* StackTrace,
+        DWORD StackTraceSize, /*in characters*/
+        DWORD MaxStackTraceDepth);
+};
+
+#endif // #if IE_PLATFORM_WINDOWS
+
+#endif // __RPC_EX_STACK_TRACE_ENGINE_INCLUDED__
