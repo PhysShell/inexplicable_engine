f0b992767a5baf4ee890d9b13bdeb6f45cc0d771
PhysShell <schelipov.fedor@yandex.ru>
2018-12-23 22:24:42 +0800

    * UPD: shared_object is now in memory namespace, implementation been moved to *_inline.h

---
+++ inex/core/sources/memory_shared_object.h
@@ -0,0 +1,72 @@
+#ifndef SHARED_OBJECT_H_INCLUDED
+#   define SHARED_OBJECT_H_INCLUDED
+
+#   define IE_USE_CRITICAL_SECTIONS_IN_SHARED_OBJECT
+
+namespace inex {
+namespace core {
+namespace memory {
+
+template < typename T >
+class shared_object 
+{
+public:
+    typedef shared_object < T >     self_type;
+// to make static member which look for memory leaks ( very simple )
+public:
+	//-----------------------------------------------------------------------------------
+	// initializations
+	//-----------------------------------------------------------------------------------
+                    shared_object   ( );
+                    shared_object   ( self_type const& owner );
+    explicit        shared_object   ( T* const data );
+                    shared_object   ( self_type&& ) =   delete;
+                    ~shared_object  ( ); 
+
+	//-----------------------------------------------------------------------------------
+	// operations
+	//-----------------------------------------------------------------------------------
+    size_t          owners          ( ) const;
+    bool            unique          ( ) const;
+    void            reset           ( T* const data );
+
+    explicit        operator bool   ( )
+                    {
+                        return      m_data;
+                    }
+
+	//-----------------------------------------------------------------------------------
+	// accessors
+	//-----------------------------------------------------------------------------------
+    T* const        get             ( );
+    const T* const  get             ( ) const;
+    // m_data should be const TYPE to const operator * version to be called
+    T const&        operator *      ( ) const;
+    T&              operator *      ( );
+
+private:
+	//-----------------------------------------------------------------------------------
+	// private
+	//-----------------------------------------------------------------------------------
+    size_t          decrement       ( );
+    void            release         ( );
+    void            increment       ( );
+
+private:
+    size_t* m_owners;
+    T*      m_data;
+}; // class shared_object
+
+//-----------------------------------------------------------------------------------
+// shared_object outer operators
+//-----------------------------------------------------------------------------------
+template < typename T >
+bool	operator == ( shared_object< T > const& lhs,	shared_object< T > const& rhs );
+
+} // namespace memory
+} // namespace core
+} // namespace inex
+
+#	include "memory_shared_object_inline.h"
+
+#endif // #ifndef SHARED_OBJECT_H_INCLUDED
---
+++ inex/core/sources/memory_shared_object_inline.h
@@ -0,0 +1,175 @@
+#ifndef MEMORY_SHARED_OBJECT_INLINE_H_INCLUDED
+#	define  MEMORY_SHARED_OBJECT_INLINE_H_INCLUDED
+
+namespace inex {
+namespace core {
+namespace memory {
+
+#	define	TEMPLATE_SPECIALIZATION	template < typename T >
+#	define	SHARED_OBJECT_TEMPLATE shared_object< T >
+
+TEMPLATE_SPECIALIZATION
+inline
+SHARED_OBJECT_TEMPLATE::shared_object ( ) :
+    m_owners    ( nullptr ),
+    m_data      ( nullptr )
+{	 //LOGGER (	"+\tDEFAULT ctor:\t %p\n\tm_data:\t\t %p\n\tm_owners:\t\t%d\n", this, m_data, m_owners ? *m_owners : 0 ); 
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+SHARED_OBJECT_TEMPLATE::shared_object ( T* const data ) :
+    m_owners    (  memory::ie_new< size_t > ( 1u ) ),
+    m_data      ( data )
+{
+ 	//LOGGER (	"+\tDATA ctor:\t %p\n\tm_data:\t\t %p\n\tm_owners:\t\t%d\n", this, m_data, *m_owners );
+ 
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+SHARED_OBJECT_TEMPLATE::shared_object ( SHARED_OBJECT_TEMPLATE const& owner ) :
+    m_data      ( owner.m_data ),
+    m_owners    ( owner.m_owners )
+{
+/*     if ( !owner.m_owners )
+    {
+        //LOGGER          ( "no OWNERS COPY ctor has\n" );
+        getchar         ( );
+    } */
+
+    increment         ( );
+
+  	//LOGGER (	"+\tCOPY ctor:\t %p\n\tm_data:\t\t %p\n\tm_owners:\t\t%d\n", this, m_data, *m_owners );   
+ }
+
+TEMPLATE_SPECIALIZATION
+inline
+SHARED_OBJECT_TEMPLATE::~shared_object ( )
+{
+    if ( !m_data )
+    {
+         //LOGGER  ( "theres nothing to delete!\n" ); 
+        return ;
+    }
+
+    if ( decrement( ) < 1 )
+    {
+         //LOGGER (	"-\tDTOR FREE:\t %p\n\tm_data:\t\t %p\n\tm_owners:\t\t%d\n", this, m_data, *m_owners );   
+         release ( );
+    }
+    else
+    {
+         //LOGGER (	"-\tDTOR REF_DEC:\t %p\n\tm_data:\t\t %p\n\tm_owners:\t\t%d\n", this, m_data, *m_owners );   
+     }
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+T const&  SHARED_OBJECT_TEMPLATE::operator * ( ) const
+{
+    return              *m_data;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+void  SHARED_OBJECT_TEMPLATE::reset ( T* const data )
+{
+#   pragma message ( "review this one" )
+    if ( !data )
+    {
+		//LOGGER( "NULLPTR PASSED" );
+        release ( );
+        return              ;
+    }
+
+    if ( !m_owners )
+    {
+         //LOGGER  ( "no owners, allocate.\n" ); 
+        m_owners            = memory::ie_new< size_t > ( 1u );
+        m_data              = data;
+    }
+    else if ( unique( ) )
+    {
+         //LOGGER  ( "unique owner, reset.\n" ); 
+        memory::ie_delete	( m_data );
+        m_data              = data;
+    }
+    else
+    {
+         //LOGGER  ( "many owner, decrement and reset.\n" ); 
+        decrement           ( );
+        m_owners            = memory::ie_new< size_t > ( 1u );
+        m_data              = data;
+    }
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+T&  SHARED_OBJECT_TEMPLATE::operator * ( )
+{
+    return              *m_data;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+void  SHARED_OBJECT_TEMPLATE::release ( )
+{
+     //LOGGER ( "releasing DATA: %p\n", m_data ); 
+	
+    memory::ie_delete	( m_data );
+    memory::ie_delete	( m_owners);
+    m_owners            = nullptr;
+    m_data              = nullptr;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+size_t  SHARED_OBJECT_TEMPLATE::owners ( ) const
+{
+    return              m_owners ? *m_owners : 0;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+bool    SHARED_OBJECT_TEMPLATE::unique ( ) const
+{
+    return              *m_owners == 1;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+void    SHARED_OBJECT_TEMPLATE::increment ( )
+{
+    ++                  *m_owners;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+size_t  SHARED_OBJECT_TEMPLATE::decrement ( )
+{
+    return              --*m_owners;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+T* const    SHARED_OBJECT_TEMPLATE::get ( )
+{
+    return              m_data;
+}
+
+TEMPLATE_SPECIALIZATION
+inline
+const T* const  SHARED_OBJECT_TEMPLATE::get ( ) const
+{
+    return              m_data;
+}
+
+#undef	TEMPLATE_SPECIALIZATION
+#undef	SHARED_OBJECT_TEMPLATE
+
+} // namespace memory
+} // namespace core
+} // namespace inex
+
+#endif // #ifndef MEMORY_SHARED_OBJECT_INLINE_H_INCLUDED
