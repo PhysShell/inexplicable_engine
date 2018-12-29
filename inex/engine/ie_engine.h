a180171cfd35bf9dfda1d22a3c2f60b82526d842
PhysShell <schelipov.fedor@yandex.ru>
2018-12-29 20:44:11 +0800

    * OPT: engine does not initializes core

--- inex/engine/ie_engine.h
+++ inex/engine/ie_engine.h
@@ -1,33 +1,41 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created 	: 09.02.2018
+//	Author		: Feudor Shelipov
+////////////////////////////////////////////////////////////////////////////
+
 #ifndef IE_ENGINE_H_INCLUDED
 #	define IE_ENGINE_H_INCLUDED
 
-
+#	include <inex/core/sources/utils.h>
 
 namespace inex {
 namespace engine  {
 
-struct /*_declspec(novtable)*/  engine_base
+struct /*_declspec(novtable)*/  engine_base											
 {
-
-	virtual			~engine_base			( ) 				{ }
 	virtual void 	exit					( s32 exit_code ) = 0;
 	virtual void 	set_exit_code			( s32 exit_code ) = 0;
 	virtual int		get_exit_code			( ) const = 0;
-}; // class IEngine
+protected:
+	IE_DECLARE_PURE_VIRTUAL_DESTRUCTOR( engine_base )
+}; // struct engine_base
 
-class engine :
-	public engine_base
+class engine : 
+			core::utils::nonassignable, 
+			core::utils::noncopyable, 
+			public engine_base
 {
-
 public:
-					engine					( s32 argc , pstr* argv );
+					engine					( u32 argc , pstr* argv, pvoid instance = 0 );
+					engine					( )					{							}
 	virtual			~engine					( );
-	virtual void 	exit					( s32 error_code )	{ m_exit_code = error_code; /*xray::debug::terminate("");*/}
-	virtual int		get_exit_code			( ) const			{ return m_exit_code; }
+	virtual void 	exit					( s32 error_code )	{ m_exit_code = error_code;	}
+	virtual s32		get_exit_code			( ) const			{ return m_exit_code;		}
 	virtual void 	set_exit_code			( s32 error_code )	{ m_exit_code = error_code; }
 
+//			void	run						( );
 private:
-	s32		m_exit_code;
+	s32						m_exit_code;
 }; // class engine
 
 } //namespace inex
--- inex/engine/sources/ie_engine.cpp
+++ inex/engine/sources/ie_engine.cpp
@@ -1,23 +1,16 @@
 #include "stdafx.h"
-
 #include <inex/engine/ie_engine.h>
-#include "build_id_computer.h"
-#include <inex/core/ie_core.h>
   
 namespace inex {
 namespace engine {
 
-engine::engine (s32 argc, pstr* argv ) :
-	m_exit_code		{ 0 }
-    {
-        inex::core::initialize( argc, argv );
-        //if(strstr(inex::core::get_params(), "-engine"))
-       inex::core::log::Msg( "Initializing Engine...\n" );
-    }
+engine::engine ( u32 argc, pstr* argv, pvoid instance ) :
+	m_exit_code		( 0 )
+{
+}
 
 engine::~engine ( )
 {
-    inex::core::finalize( );
 }
 
 } // namespace inex
