b4e57b774b0b9d234984cdc15355574b2a83217b
PhysShell <schelipov.fedor@yandex.ru>
2018-12-29 22:58:35 +0800

    * BUG: win32 doesn't see wmain as entry point. stupid msvc...

--- inex/engine/sources/engine_linux_entry_point.cpp
+++ inex/engine/sources/engine_linux_entry_point.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <inex/engine/ie_engine.h>
+#include <inex/core/ie_core.h>
 
 namespace inex {
 namespace platform {
@@ -9,7 +10,7 @@ s32		engine_entry_point (	pstr		command_line_string		);
 
 s32		engine_entry_point ( pstr command_line_string )
 {
-	core::initialize	( 0, command_line_string );
+	core::initialize	( 0, 0 );
 	core::log::Msg		( "Initializing Engine...\n" );
 	engine::engine		en;
 	core::finalize		( );
@@ -19,6 +20,9 @@ s32		engine_entry_point ( pstr command_line_string )
 } // namespace platform
 } // namespace inex
 
+
+// there's some msvc bullshit which doesn't allow me to
+// use wmain as entry point for win x86, so use linux version here
 int main( int argc, pstr* argv )
 {
 	{
