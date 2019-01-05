cb858bf23363e62581712a3e63b899cc85172be1
PhysShell <schelipov.fedor@yandex.ru>
2019-01-05 19:20:26 +0800

    * NEW, windows: opengl static linkage

---
+++ inex/render/api.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////
+//	Created 	: 04.01.2019
+//	Author		: Feudor Shelipov
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef INEX_RENDER_API_H_INCLUDED
+#	define INEX_RENDER_API_H_INCLUDED
+
+
+
+namespace inex {
+namespace ui {
+
+INEX_UI_API	void	create_world	( );
+INEX_UI_API	void	destroy_world	( );
+
+} // namespace ui
+} // namespace inex
+
+#endif // #ifndef INEX_RENDER_API_H_INCLUDED
