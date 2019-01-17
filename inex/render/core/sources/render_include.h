cf120c976a10187f9b50e4d1d82f6b7939afa63b
PhysShell <schelipov.fedor@yandex.ru>
2019-01-17 20:12:20 +0800

    * UPD: kyh,kgtykgkyujm

--- inex/render/core/sources/render_include.h
+++ inex/render/core/sources/render_include.h
@@ -11,17 +11,14 @@
 // in freeglut_std.h, it says lib to be named "freeglut.lib" for static linkage, too
 #	define FREEGLUT_STATIC
 
+#	include <inex/3rd_patry/include/GL/glew.h>
+#	include <inex/3rd_patry/include/GLFW/glfw3.h>
+#	include <inex/3rd_patry/include/GL/freeglut.h>
+
 #   if IE_PLATFORM_WINDOWS
 #	    pragma comment( lib, "freeglut.lib" )
 #	    pragma comment( lib, "glew32_static.lib" )
 #	    pragma comment( lib, "glfw3_static.lib" )
-#	    include <inex/3rd_patry/include/GL/glew.h>
-#	    include <inex/3rd_patry/include/GLFW/glfw3.h>
-#	    include <inex/3rd_patry/include/GL/freeglut.h>
-#   else // #   if IE_PLATFORM_WINDOWS
-#       include <GL/glew.h>
-#       include <GLFW/glfw3.h>
-#       include <GL/freeglut.h>
 #   endif // #   if IE_PLATFORM_WINDOWS
 
 #endif // #ifndef RENDER_INCLUDE_H_INCLUDED
