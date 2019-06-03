456638c6c245abc90fd54de0e8ffa2a0a39d08ee
PhysShell <schelipov.fedor@yandex.ru>
2019-06-04 01:15:57 +0800

    * NEW: math functions

--- inex/math_functions.h
+++ inex/math_functions.h
@@ -9,13 +9,54 @@
 namespace inex {
 namespace math {
 
-template < typename T > inline
+#define PI              3.1415926535897932384626433832795f
+
+template < typename T > constexpr inline
+T   abs_universal ( T value )
+{
+    return              value > ( T )0 ? value : -value;
+}
+
+#define fdEPS           machine_epsilon( )
+constexpr inline
+float   machine_epsilon ( )
+{
+	float e             = 1.0f;
+	while ( 1.0f + e / 2.0f > 1.0f ) 		e       /= 2.0f;
+	return              e;
+}
+
+template < typename T > constexpr inline
 T   sqr ( T const& value )   {   return ( value * value );  }
 
-template < typename T > inline
-T   max2 ( T const& left, T const& right )   
+template < typename T > constexpr inline
+T   max ( T const& left, T const& right )   
 {  
     return              left > right ? left : right;
+}
+
+constexpr inline
+float   degrees_to_radians ( float const value )
+{
+    return              value / 180.f * PI;
+}
+
+constexpr inline
+float   radians_to_degree ( float const value )
+{
+    return              value / PI * 180.f;
+}
+
+constexpr inline
+float   sin ( float const angle )
+{
+    return              sinf( angle );
+}
+
+constexpr inline
+float   cos ( float const angle )
+{
+    return              cosf( angle );
 }
 
 } // namespace math
