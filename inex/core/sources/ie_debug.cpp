e6cf01de4c52bdd1f377b5b57e993b8e586def90
PhysShell <mouse.kcschelipov.fedor@yandex.ru>
2021-07-25 20:37:31 +0600

    * FIX: condition compile for linux

--- inex/core/sources/ie_debug.cpp
+++ inex/core/sources/ie_debug.cpp
@@ -234,44 +234,48 @@ void	fatal ( pcstr 	file,
         logging::put_string		( "Description:\t<no description>\n" );
     }
 
-        Display *d;
-        Window w;
-        XEvent e;
-        int s;
+	logging::put_string			( "***\nStack Dump:\n***\n" );
+	dump_call_stack_trace	    ( );
+	memory::dump_memory_contents( );
 
-        d = XOpenDisplay(NULL);
-        if ( !d ) {
-                fprintf(stderr, "Cannot open display\n");
-                exit(1);
-        }
+#if INEX_PLATFORM_LINUX
+    Display *   d;
+    Window      w;
+    XEvent      e;
+    int         s;
 
-        s = DefaultScreen(d);
-        w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
-                                BlackPixel(d, s), WhitePixel(d, s));
-        XSelectInput(d, w, ExposureMask | ButtonPressMask );
+    d = XOpenDisplay(NULL);
+    if ( !d ) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
+    }
 
-        XStoreName      ( d, w, "UNHANDLED EXCEPTION" );
-        XMapWindow(d, w);
+    s = DefaultScreen(d);
+    w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
+                            BlackPixel(d, s), WhitePixel(d, s));
+    XSelectInput(d, w, ExposureMask | ButtonPressMask );
 
-        for ( ; ; ) {
-        XNextEvent(d, &e);
-        if ( e.type == Expose ) {
+    XStoreName      ( d, w, "UNHANDLED EXCEPTION" );
+    XMapWindow(d, w);
+
+    for ( ; ; )
+    {
+        pcstr const string      = "FIX ME";
+        XNextEvent  (d, &e);
+        if ( e.type == Expose )
+        {
                 // XFillRectangle (d, w, DefaultGC(d, s), 20, 20, 10, 10);
-                XDrawString    (d, w, DefaultGC(d, s), 10, 50, "buf", sizeof( "buf" ) - 1);
+                XDrawString    (d , w, DefaultGC( d, s ), 10, 50, string, sizeof( string ) - 1);
         }
+
         if ( e.type == ButtonPressMask )
                 break;
-        }
+    }
 
-        //XFreeGC         ( d,  gc );
-        XDestroyWindow  ( d, w );
-        XCloseDisplay(d);
-
-
-
-	logging::put_string			( "***\nStack Dump:\n***\n" );
-	dump_call_stack_trace	( );
-	memory::dump_memory_contents( );
+    //XFreeGC         ( d,  gc );
+    XDestroyWindow  ( d, w );
+    XCloseDisplay   ( d );
+#endif // #if INEX_PLATFORM_LINUX
 
 // 	if ( s_display )
 //     {
